5d0af62363fc1f65a1ba77f58fc90fb7c3449df2
Renzo1818 <93232895+Renzo1818@users.noreply.github.com>
2023-01-03 17:31:19 -0500

    Update Tablas.sql

--- Implementación de DB/Tablas.sql
+++ Implementación de DB/Tablas.sql
@@ -45,3 +45,37 @@ Utilidades double not null,
 primary key(CodAdmin),
 foreign key(CodAdmin) references Trabajador(CodTrabajador)
 );
+
+
+/*CREACION DE TABLA USUARIO*/
+
+use db_ventas;
+Create table Usuario(
+CodUsuario int not null,
+Nombre_Usu varchar(50) not null unique,
+Contraseña varchar(30) not null,
+Tipo_Usu varchar(30) not null,
+
+primary key(CodUsuario),
+foreign key(CodUsuario) references Trabajador(CodTrabajador)
+);
+
+
+/*CREACION DE TABLA CLIENTE*/
+
+create table Cliente(
+CodCliente int not null,
+Nombre varchar(30) not null,
+Apellido varchar(30) not null,
+DNI varchar(8) not null unique,
+Ruc varchar(11),
+Sexo varchar(10) not null,
+Fecha_Nacimiento date not null,
+Email varchar(50) not null unique,
+Direccion varchar(50) not null,
+Distritos varchar(50) not null,
+Provincia varchar(50) not null,
+Pais varchar(30) not null,
+
+primary key(CodCliente)
+);
