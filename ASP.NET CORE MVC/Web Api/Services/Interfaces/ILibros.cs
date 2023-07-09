f2abf503fbc89d4ab4f245eb17b8591f85befad8
Renzo1818 <93232895+Renzo1818@users.noreply.github.com>
2023-07-08 21:01:30 -0500

    Add files via upload

---
+++ ASP.NET CORE MVC/Web Api/Services/Interfaces/ILibros.cs
@@ -0,0 +1,13 @@
+﻿using API_BIBLIO.Models;
+
+namespace API_BIBLIO.Services.Interfaces
+{
+    public interface ILibros
+    {
+        IEnumerable<ConsolidadoLibro> getAllConsolidado();
+        ConsolidadoLibro getId(int id);
+        void add(Libro libro);
+        void update(Libro libro);
+        void delete(int id);
+    }
+}
---
+++ ASP.NET CORE MVC/Web Api/Services/Repository/LibrosRespository.cs
@@ -0,0 +1,44 @@
+﻿using API_BIBLIO.Models;
+using API_BIBLIO.Services.Interfaces;
+
+namespace API_BIBLIO.Services.Repository
+{
+    public class LibrosRespository:ILibros
+    {
+        OperaC conexion = new OperaC();
+        public IEnumerable<ConsolidadoLibro> getAllConsolidado()
+        {
+            return conexion.ConsolidadoLibros;
+        }
+
+        public ConsolidadoLibro getId(int id)
+        {
+            var obj = conexion.ConsolidadoLibros.Where(x => x.IdLibro == id).FirstOrDefault();
+            return obj;
+        }
+
+        public void add(Libro libro)
+        {
+            conexion.Libros.Add(libro);
+            conexion.SaveChanges();
+        }
+
+        public void delete(int id)
+        {
+            conexion.Remove(getId(id));
+        }
+
+        public void update(Libro libro)
+        {
+            var objModificar = (from tbLibro in conexion.Libros where tbLibro.IdLibro == libro.IdLibro select tbLibro).FirstOrDefault();
+            objModificar.Titulo = libro.Titulo;
+            objModificar.IdCategoria = libro.IdCategoria;
+            objModificar.IdAutor = libro.IdAutor;
+            objModificar.IdEditorial = libro.IdEditorial;
+            objModificar.Stock += libro.Stock;
+            objModificar.FechaPublicacion = libro.FechaPublicacion;
+
+            conexion.SaveChanges();
+        }
+    }   
+}
