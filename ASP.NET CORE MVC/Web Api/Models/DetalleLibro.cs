6b21134e9009274beeb8fb5eceb1aa03226c94c3
Renzo1818 <93232895+Renzo1818@users.noreply.github.com>
2023-07-08 21:01:11 -0500

    Add files via upload

---
+++ ASP.NET CORE MVC/Web Api/Models/DetalleLibro.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API_BIBLIO.Models;
+
+public partial class DetalleLibro
+{
+    public int IdLibro { get; set; }
+
+    public string Sinopsis { get; set; } = null!;
+
+    public virtual Libro IdLibroNavigation { get; set; } = null!;
+}
