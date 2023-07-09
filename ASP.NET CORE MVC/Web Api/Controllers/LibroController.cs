36210a645797156abdfb707935893c205d52ca2d
Renzo1818 <93232895+Renzo1818@users.noreply.github.com>
2023-07-08 21:00:43 -0500

    Add files via upload

---
+++ ASP.NET CORE MVC/Web Api/Controllers/LibroController.cs
@@ -0,0 +1,64 @@
+﻿using API_BIBLIO.Models;
+using API_BIBLIO.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_BIBLIO.Controllers
+{
+    [Route("Api/[Controller]")]
+    [ApiController]
+    public class LibroController : Controller
+    {
+
+        private readonly ILibros _libros;
+        public LibroController(ILibros libros)
+        {
+            _libros = libros;
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_libros.getAllConsolidado());
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var obj = _libros.getId(id);
+            if (obj == null)
+            {
+                return NotFound("El Libro(" + id.ToString() + ") no existe");
+            }
+            else
+            {
+                return Ok(obj);
+            }
+        }
+
+
+        [HttpPost("agregar")]
+        public IActionResult add(Libro libro)
+        {
+            _libros.add(libro);
+            return CreatedAtAction(nameof(add), libro);
+        }
+
+
+        [HttpDelete("eliminar/{id}")]
+        public IActionResult delete(int id)
+        {
+            _libros.delete(id);
+            return NoContent();
+        }
+
+
+        [HttpPut("modificar")]
+        public IActionResult modificar(Libro libro)
+        {
+            _libros.update(libro);
+            return CreatedAtAction(nameof(modificar), libro);
+        }
+    }
+}
